463030f27225c07acfdaf545775aa092e918c90b
Uriel <urielhershko@gmail.com>
2023-08-18 19:21:50 +0300

    added a button in navbar called: home and better looking forms for register and login

--- GameWeb/urls.py
+++ GameWeb/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
    path('logout/', views.logout_user , name = "logout"),
    path('register/', views.register, name='register'),
    path('home/', views.home , name = "home"),
-   path('game/', views.games_text , name = "game-text"),
+   path('game/', views.games, name = "game-text"),
    path('place_order/<int:game_id>/', views.place_order, name='place-order')
 ]
