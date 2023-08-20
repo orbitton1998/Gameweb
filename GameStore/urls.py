036c3dab097ef0c99c80c91802c124138797dff4
Uriel <urielhershko@gmail.com>
2023-08-20 20:30:13 +0300

    added clean path in urlpatterns

--- GameStore/urls.py
+++ GameStore/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('GameWeb/home', include ('GameWeb.urls'))
+    path('', include ('GameWeb.urls'))
 ]
